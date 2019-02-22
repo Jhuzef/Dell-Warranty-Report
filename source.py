5010ad6d459fff8e011c61b84a6c263fba0a3ed2
jhuzef <josephgonzales034@gmail.com>
2019-02-22 11:02:03 -0500

    Initial Commit

---
+++ source.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+__author__ = "Joseph L. Gonzales"
+__email__ = "joseph.gonzales@konicaminolta.com"
+__server__ = ""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
